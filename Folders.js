d648516394f2dd9354bdc59a7d41db9387e29d76
Cody Ruff <176826713+codyruff@users.noreply.github.com>
2026-03-16 16:18:38 -0400

    worked on implementing the add folders function/button as object oriented js

--- Folders.js
+++ Folders.js
@@ -1,21 +1,34 @@
-function createFolder(name) {
-
-    const folder = document.createElement('div');
-    folder.className = 'folder';
-    folder.innerText = name;
-    document.querySelector('#folders').appendChild(folder);
+class folder{
 
 
 
+    constructor(containerElement, PassedFolderName) {
+        const containerElement = containerElement;
+        const folderDiv = document.createElement('div');
+        folderDiv.className = 'folder';
+        folderDiv.innerText = PassedFolderName;
+        document.querySelector('#folders').appendChild(folderDiv);
+        this.createFolderInHTML = this.createFolderInHTML.bind(this);
+
+
+        document.querySelector("#create-folder").addEventListener("click", this.createFolderhandler());
+
+    }
+
+    createFolderhandler(event){
+        event.preventDefault();
+        this.createFolderInHTML();
+    }
+
+    createFolderInHTML(){
+        const folderButton = this.document.createElement("button");
+        folderButton.classList.add("Folder-Button");
+        button.type = "button";
+        button.textContent = this.PassedFolderName;
+        document.querySelector(this.folderDiv).appendChild(folderButton)
+
+
+    }
+    
 }
-
-function createFolderhandler(event){
-    event.preventDefault();
-
-}
-
-function setup() {
-    document.querySelector("#create-folder").addEventListener("click", createFolderhandler);
-}
-
 export default setup;
