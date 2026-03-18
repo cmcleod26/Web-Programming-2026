801ef0fcb5767c20bb10f395b603e9c8e3bc3ae8
Connor McLeod <cmcleod2@students.stonehill.edu>
2026-03-18 16:17:15 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+/*
+app is our express application
+this is how we define routes, middleware and start the server
+*/ 
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000; //default value = 5000
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI).then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.log('Error connected to MongoDB', error);
+    });
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+
+
