959cb5a79691255e961b69a3cddfb9b04cac5971
Connor McLeod <cmcleod2@students.stonehill.edu>
2026-03-23 16:09:15 -0400

    final routes for files

--- server/routes/fileRoutes.js
+++ server/routes/fileRoutes.js
@@ -39,16 +39,29 @@ router.put('/:id', async(req, res) => {
             return res.status(401).json({message: 'Incorrect Password'});
         }
         const file = await File.findByIdAndUpdate(req.params.id, { $set: { name: name, content: content } }, {new:true});
-
         if (!file){
             return res.status(404).json({message: 'file not found'});
         }
-
         res.json(file);
     } catch (error) {
         res.status(500).json({message: error.message});
     }
-    
+});
+
+router.delete('/:id', async(req,res) => {
+    try {
+        const {password} = req.body;
+        if(password !== process.env.COMMIT_PASSWORD){
+            return res.status(401).json({message: 'Incorrect Password'});
+        }
+        const file = await File.findByIdAndDelete(req.params.id);
+        if (!file){
+            return res.status(404).json({message: 'file not found'});
+        }
+        res.json({ message: 'File deleted' });
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 });
 
 module.exports = router;
