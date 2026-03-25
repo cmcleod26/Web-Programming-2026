2351304ff4251b353ce46bedd4b3d87b62d02217
Connor McLeod <cmcleod2@students.stonehill.edu>
2026-03-25 01:36:54 -0400

    updated readme

--- server/server.js
+++ server/server.js
@@ -13,8 +13,11 @@ this is how we define routes, middleware and start the server
 app.use(cors());
 app.use(express.json());
 
-const folderRoutes = require('./server/routes/fodlerRoutes');
-app.use('/api/folders', folderRoutes)
+const folderRoutes = require('./routes/folderRoutes');
+app.use('/api/folders', folderRoutes);
+
+const fileRoutes = require('./routes/fileRoutes');
+app.use('/api/files', fileRoutes);
 
 const PORT = process.env.PORT || 5000; //default value = 5000
 const MONGO_URI = process.env.MONGO_URI;
